76d241d41f7fd311c23911e4f72915e614efab7a
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-08-05 22:34:07 -0400

    sim (WIP): More progress on the C++ frontend

--- ocaml/cpp.ml
+++ ocaml/cpp.ml
@@ -1,20 +1,17 @@
-type register_t = {
-    r_name: string;
-    r_size: int;
-    r_type: string;
-  }
+open Common
 
-type rule_t = {
+type ('reg_t, 'fn_t) rule_t = {
     rl_name: string;
-    rl_footprint: register_t list;
-    rl_body: unit;
+    rl_footprint: 'reg_t list;
+    rl_body: (string, 'reg_t, 'fn_t) SGALib.SGA.rule;
   }
 
-type h_file_t = {
-    h_preamble: string;
+type ('prim, 'reg_t, 'fn_t) cpp_input_t = {
     h_classname: string;
-    h_rules: rule_t list;
-    h_registers: register_t list
+    h_rules: ('reg_t, 'fn_t) rule_t list;
+    h_register_names: 'reg_t list;
+    h_registers: 'reg_t -> reg_signature;
+    h_functions: 'fn_t -> 'prim ffi_signature
   }
 
 let sprintf = Printf.sprintf
@@ -23,7 +20,7 @@ let fprintf = Printf.fprintf
 let cpp_type_of_size sz =
   assert (sz >= 0);
   if sz = 0 then
-    "prims::unit_t"
+    "prims::unit_t"             (* FIXME *)
   else if sz = 1 then
     "std::bool"
   else if sz <= 8 then
@@ -34,22 +31,57 @@ let cpp_type_of_size sz =
     "std::uint32_t"
   else if sz <= 64 then
     "std::uint64_t"
-  (* The following two are not universally supported *)
-  (* else if sz <= 128 then
-   *   "std::uint128_t"
-   * else if sz <= 256 then
-   *   "std::uint256_t" *)
+  else
+    (* The following two are not universally supported *)
+    (* else if sz <= 128 then
+     *   "std::uint128_t"
+     * else if sz <= 256 then
+     *   "std::uint256_t" *)
+    failwith (Printf.sprintf "Unsupported size: %d" sz)
+
+let cpp_const_init_fn sz =
+  assert (sz >= 0);
+  if sz = 0 then
+    "UNIT"             (* FIXME *)
+  else if sz = 1 then
+    "std::bool"
+  else if sz <= 8 then
+    "UINT8_C"
+  else if sz <= 16 then
+    "UINT16_C"
+  else if sz <= 32 then
+    "UINT32_C"
+  else if sz <= 64 then
+    "UINT64_C"
   else
     failwith (Printf.sprintf "Unsupported size: %d" sz)
+
+let h_preamble =
+  "FIXME: PREAMBLE"
+
+let gensym =
+  let state = Hashtbl.create 8 in
+  fun prefix ->
+  let counter =
+    match Hashtbl.find_opt state prefix with
+    | None -> 0
+    | Some n -> n in
+  if counter = max_int then failwith "gensym";
+  Hashtbl.replace state prefix (succ counter);
+  sprintf "%s%d" prefix counter
 
 let writeout out hpp =
   let nl _ = output_string out "\n" in
   let p fmt = Printf.kfprintf nl out fmt in
+  let pr fmt = Printf.fprintf out fmt in
 
-  let p_fn typ name ?args:(args="") ?annot:(annot="") pbody =
-    p "%s %s(%s)%s {" typ name args annot;
+  let p_scoped header pbody =
+    p "%s {" header;
     pbody ();
     p "}" in
+
+  let p_fn typ name ?args:(args="") ?annot:(annot="") pbody =
+    p_scoped (sprintf "%s %s(%s)%s" typ name args annot) pbody in
 
   let p_ifdef pbody =
     let h_define = sprintf "__%s_HPP__" hpp.h_classname in
@@ -64,7 +96,7 @@ let writeout out hpp =
     p "// PREAMBLE //";
     p "//////////////";
     nl ();
-    p "%s" hpp.h_preamble in
+    p "%s" h_preamble in
 
   let p_impl () =
     p "////////////////////";
@@ -72,35 +104,107 @@ let writeout out hpp =
     p "////////////////////";
 
     let p_class pbody =
-      p "class %s {" hpp.h_classname;
-      pbody ();
-      p "}" in
+      p_scoped (sprintf "class %s" hpp.h_classname) pbody in
 
-    let p_state_register r =
-      p "%s %s;" r.r_type r.r_name in
+    let p_state_register rn =
+      let r = hpp.h_registers rn in
+      p "%s %s;" (cpp_type_of_size r.reg_size) r.reg_name in
 
     let p_state_t () =
-      let p_printf_register r =
-        p "std::cout << \"%s = \" << %s << std::endl;"
-          r.r_name r.r_name in
-      p "struct state_t {";
-      List.iter p_state_register hpp.h_registers;
-      nl ();
-      p_fn "void" "dump" (fun () ->
-        List.iter p_printf_register hpp.h_registers);
-      p "}" in
+      let p_printf_register rn =
+        let name = (hpp.h_registers rn).reg_name in
+        p "std::cout << \"%s = \" << %s << std::endl;" name name in
+      p_scoped "struct state_t" (fun () ->
+          List.iter p_state_register hpp.h_register_names;
+          nl ();
+          p_fn "void" "dump" (fun () ->
+              List.iter p_printf_register hpp.h_register_names)) in
+
+    let p_expr sz var expr =
+      let p_assign sz target prexpr =
+        pr "%s %s = " (cpp_type_of_size sz) target;
+        prexpr ();
+        pr ";";
+        nl () in
+
+      let pr_const bits =
+        let b = SGALib.Util.bits_const_of_bits bits in
+        1 in
+
+      let p_expr' sz var expr = function
+        | SGA.Var (v, sz', _) ->
+           assert (sz = sz');
+           p_assign sz var (fun () -> pr "%s" v)
+        | SGA.Const (sz, cst) ->
+           p_assign sz var (fun () ->
+               pr "%s(" (cpp_const_init_fn sz);
+               pr_const cst;
+               pr ")")
+        | SGA.Read (port, reg) ->
+           let r = hpp.h_registers reg in
+           let fn_name = match port with
+             | P0 -> "read0"
+             | P1 -> "read1" in
+           assert (sz = r.reg_size);
+           p "%s %s;" (cpp_type_of_size sz) var;
+           p_checked (fun () ->
+               pr "log.%s.%s(&%s, Log.%s)"
+                 r.reg_name fn_name var r.reg_name)
+        | SGA.Call (fn, arg1, arg2) ->
+           let v1 = gensym "arg1_" in
+           let v2 = gensym "arg2_" in
+           let f = hpp.h_functions fn in
+           p_expr f.ffi_arg1size v1 arg1;
+           p_expr f.ffi_arg2size v2 arg2;
+           p_assign sz var (fun () ->
+               pr "%s(%s, %s)" (cpp_fn_name f.ffi_name) v1 v2)
+      in p_expr' sz var expr'
 
     let p_rule rule =
+      let p_checked prbody =
+        pr "CHECK_RETURN(";
+        prbody ();
+        pr ")" in
+
       let p_reset () =
-          List.iter (fun r ->
-              p "log.%s.reset(Log.%s);" r.r_name r.r_name)
-            rule.rl_footprint in
+        List.iter (fun { reg_name; _ } ->
+            p "log.%s.reset(Log.%s);" reg_name reg_name)
+          rule.rl_footprint in
+
       let p_commit () =
-          List.iter (fun r ->
-              p "Log.%s = log.%s;" r.r_name r.r_name)
-            rule.rl_footprint in
-      let p_rule_body body =
-        p "FIXME" in
+        List.iter (fun { reg_name; _ } ->
+            p "Log.%s = log.%s;" reg_name reg_name)
+          rule.rl_footprint in
+
+      let rec p_rule_body = function
+        | SGA.Skip _ ->
+           p ""
+        | SGA.Fail _ ->
+           p "return;"
+        | SGA.Seq (_, r1, r2) ->
+           p_rule_body r1;
+           p_rule_body r2
+        | SGA.Bind (_, sz, v, ex, rl) ->
+           (* FIXME make sure name doesn't start with our prefix *)
+           p_scoped "/* bind */" (fun () ->
+               p_expr sz v ex;
+               p_rule_body rl)
+        | SGA.If (_, e, r1, r2) ->
+           p_scoped "/* if */" (fun () ->
+               let c = gensym "cond" in
+               p_expr 1 c e;
+               p_scoped (sprintf "if (%s)" c) (fun () -> p_rule_body r1);
+               p_scoped "else" (fun () -> p_rule_body r2))
+        | SGA.Write (_, port, reg, expr) ->
+           let e = gensym "write_expr" in
+           let r = hpp.h_registers reg in
+           p_expr r.reg_size e expr;
+           let fn_name = match port with
+             | P0 -> "write0"
+             | P1 -> "write1" in
+           p_checked (fun () ->
+               pr "log.%s.%s(%s, Log.%s)"
+                 r.reg_name fn_name e r.reg_name) in
       p_fn "void" rule.rl_name (fun () ->
           p_reset ();
           nl ();
@@ -119,9 +223,8 @@ let writeout out hpp =
 
     let p_run () =
       p_fn "void" "run" ~args:"std::uint64_t ncycles" (fun () ->
-          p "for (std::uint64_t cycle_id = 0; cycle_id < ncycles; cycle_id++) {";
-          p "  cycle();";
-          p "}") in
+          p_scoped "for (std::uint64_t cycle_id = 0; cycle_id < ncycles; cycle_id++)"
+            (fun () -> p "  cycle();")) in
 
     let p_observe () =
       p_fn "void" "observe" (fun () -> p "return state;") in
@@ -153,10 +256,5 @@ let writeout out hpp =
       p_impl ();
       nl ())
 
-let main (out: out_channel) (cu: SGALib.Compilation.compile_unit) =
-  writeout out {
-      h_preamble = "FIXME";
-      h_classname = failwith "FIXME";
-      h_rules = failwith "FIXME";
-      h_registers = failwith "FIXME";
-    }
+let main (out: out_channel) (cu: _ cpp_input_t) =
+  writeout out cu
