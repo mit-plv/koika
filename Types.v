423d900128a10c5f2fc17015d351c419d829618e
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-09-06 10:52:54 -0400

    coq/types: Change type; implement value_of_bits and bits_of_value

--- Types.v
+++ Types.v
@@ -1,11 +1,10 @@
-Require Export SGA.Common SGA.Vect.
 Require Export Coq.Strings.String.
 Require Import Coq.Vectors.Vector.
+Require Export SGA.Common SGA.Vect.
 
 Record struct_sig' {A} :=
-  { sv_len: nat;
-    sv_name: string;
-    sv_data: Vector.t (string * A) sv_len }.
+  { sv_name: string;
+    sv_data: list (string * A) }.
 Arguments struct_sig' : clear implicits.
 
 Inductive type : Type :=
@@ -14,10 +13,38 @@ Inductive type : Type :=
 
 Notation struct_sig := (struct_sig' type).
 
+Ltac simple_eq :=
+  first [ left; solve[eauto] | right; inversion 1; subst; solve[congruence] ].
+
+Instance EqDec_type : EqDec type.
+Proof.
+  econstructor.
+  fix IHtau 1;
+    destruct t1 as [ sz1 | fs1 ];
+    destruct t2 as [ sz2 | fs2 ]; cbn;
+      try simple_eq.
+  - destruct (eq_dec sz1 sz2); subst;
+      simple_eq.
+  - destruct fs1 as [ nm1 f1 ];
+      destruct fs2 as [ nm2 f2 ]; cbn.
+    + destruct (eq_dec nm1 nm2); subst;
+        try simple_eq.
+      revert f1 f2;
+        fix IHf 1;
+        destruct f1 as [ | (n1 & tau1) f1 ], f2 as [ | (n2 & tau2) f2 ];
+        try simple_eq.
+      destruct (eq_dec n1 n2); subst;
+        try simple_eq.
+      destruct (IHtau tau1 tau2); subst;
+        try simple_eq.
+      destruct (IHf f1 f2) as [ Heq | Hneq ]; subst;
+        try inversion Heq; try simple_eq.
+Defined.
+
 Fixpoint type_sz tau :=
   match tau with
   | bits_t sz => sz
-  | struct_t fields => Vector.fold_left (fun acc '(_, tau') => acc + type_sz tau') 0 fields.(sv_data)
+  | struct_t fields => List.fold_right (fun '(_, tau') acc => type_sz tau' + acc) 0 fields.(sv_data)
   end.
 
 Coercion type_sz : type >-> nat.
@@ -26,10 +53,79 @@ Fixpoint type_denote tau : Type :=
   match tau with
   | bits_t sz => bits sz
   | struct_t fields =>
-    Vector.fold_left (fun acc '(_, tau) => type_denote tau * acc)%type unit fields.(sv_data)
+    List.fold_right (fun '(_, tau) acc => type_denote tau * acc)%type unit fields.(sv_data)
   end.
 
 Coercion type_denote : type >-> Sortclass.
+
+Definition bits_of_value {tau: type} (x: type_denote tau) : bits (type_sz tau).
+Proof.
+  revert tau x.
+  fix IHt 1; destruct tau as [sz | sig]; cbn.
+  - intro x; exact x.
+  - destruct sig; cbn.
+    revert sv_data0.
+    fix IH 1. destruct sv_data0 as [ | (nm & tau) sv_data0 ]; cbn.
+    + intro x; exact x.
+    + intros (x & xs).
+      apply Bits.app.
+      apply (IHt tau x).
+      apply (IH _ xs).
+Defined.
+
+Program Definition value_of_bits {tau: type} (bs: bits (type_sz tau)) : type_denote tau.
+Proof.
+  revert tau bs.
+  fix IHt 1; destruct tau as [sz | sig]; cbn.
+  - intro x; exact x.
+  - destruct sig; cbn.
+    revert sv_data0.
+    fix IH 1. destruct sv_data0 as [ | (nm & tau) sv_data0 ]; cbn.
+    + intro x; exact x.
+    + intros v.
+      apply Bits.split in v.
+      exact (IHt _ (fst v), IH _ (snd v)).
+Defined.
+
+Ltac fold_fixes :=
+  repeat match goal with
+         | [  |- context[?x] ] => is_fix x; set x
+         end.
+
+Definition bits_of_value_of_bits :
+  forall tau (bs: bits (type_sz tau)),
+    bits_of_value (value_of_bits bs) = bs.
+Proof.
+  fix IHt 1; destruct tau as [sz | sig]; cbn.
+  - reflexivity.
+  - destruct sig; cbn.
+    revert sv_data0.
+    fix IH 1. destruct sv_data0 as [ | (nm & tau) sv_data0 ]; cbn.
+    + reflexivity.
+    + cbn.
+      intros. rewrite IH, IHt.
+      apply vect_app_split.
+Qed.
+
+Definition value_of_bits_of_value :
+  forall tau (v: type_denote tau),
+    (value_of_bits (bits_of_value v)) = v.
+Proof.
+  fix IHt 1; destruct tau as [sz | sig]; cbn.
+  - reflexivity.
+  - destruct sig; cbn.
+    revert sv_data0.
+    fix IH 1. destruct sv_data0 as [ | (nm & tau) sv_data0 ]; cbn.
+    + reflexivity.
+    + cbn.
+      intros.
+      rewrite (surjective_pairing v).
+      unfold Bits.split, Bits.app.
+      rewrite vect_split_app.
+      cbn.
+      rewrite IH, IHt.
+      reflexivity.
+Qed.
 
 Record ExternalSignature :=
   FunSig { arg1Type: type; arg2Type: type; retType: type }.
