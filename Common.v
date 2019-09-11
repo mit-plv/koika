1a61088d78f08e0c04b2f2f348a698745389ae2a
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-09-11 10:10:50 -0400

    coq: Add a nat to string converter

--- Common.v
+++ Common.v
@@ -177,4 +177,31 @@ Notation "'let/res' var ':=' expr 'in' body" :=
    end)
     (at level 200).
 
+Section string_of_nat.
+  Lemma digit_lt_base m {n} : not (m + n < m).
+  Proof.
+    red; intros; eapply Le.le_Sn_n; eauto using Le.le_trans, Plus.le_plus_l.
+  Qed.
+
+  Definition string_of_base10_digit (n: {n | n < 10}) :=
+    match n with
+    | exist _ 0 _ => "0" | exist _ 1 _ => "1" | exist _ 2 _ => "2" | exist _ 3 _ => "3" | exist _ 4 _ => "4"
+    | exist _ 5 _ => "5" | exist _ 6 _ => "6" | exist _ 7 _ => "7" | exist _ 8 _ => "8" | exist _ 9 _ => "9"
+    | exist _ n pr => False_rect _ (digit_lt_base 10 pr)
+    end%string.
+
+  Fixpoint string_of_nat_rec (fuel: nat) (n: nat) :=
+    match fuel with
+    | O => ""%string
+    | S fuel =>
+      match (Compare_dec.lt_dec n 10) with
+      | left pr  => string_of_base10_digit (exist _ n pr)
+      | right pr => append (string_of_nat_rec fuel (PeanoNat.Nat.div n 10)) (string_of_nat_rec fuel (PeanoNat.Nat.modulo n 10))
+      end
+    end.
+
+  Definition string_of_nat (n: nat) :=
+    string_of_nat_rec (S n) n.
+End string_of_nat.
+
 Axiom __magic__ : forall {A}, A.
