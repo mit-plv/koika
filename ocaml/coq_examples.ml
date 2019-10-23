38272d3294b23e50b3e080cfcca669b76a587da4
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-10-23 17:37:03 -0400

    ocaml: Let users specify which demos to compile in coq_examples.ml

--- ocaml/coq_examples.ml
+++ ocaml/coq_examples.ml
@@ -20,6 +20,11 @@ let coq_main ?(sim = true) ?(verilog = true) ({ vp; sp; _ }: SGA.demo_package_t)
   if sim then do_sim sp;
   if verilog then do_verilog vp
 
+let package_matches filter (p: SGA.demo_package_t) =
+  let modname = SGALib.Util.string_of_coq_string p.sga.sga_module_name in
+  try ignore (Str.search_forward filter modname 0); true
+  with Not_found -> false
+
 let mkdir dirname =
   try Unix.mkdir dirname 0o644
   with Unix.Unix_error(Unix.EEXIST, _, _) -> ()
@@ -27,4 +32,6 @@ let mkdir dirname =
 let _ =
   let open SGALib.SGA in
   mkdir "examples"; mkdir "examples/coq";
-  List.iter coq_main demo_packages
+  let filter = Str.regexp (try Sys.argv.(1) with Invalid_argument _ -> "") in
+  let packages = List.filter (package_matches filter) demo_packages in
+  List.iter coq_main packages
