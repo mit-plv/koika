c8cff36264894f7770e6e53267e57b6c5c3d8782
Thomas Bourgeat <bthom@mit.edu>
2019-10-28 20:15:45 -0400

    coq: Fix wrong parenthesis in notations for comparisons.

--- Notations.v
+++ Notations.v
@@ -117,14 +117,14 @@ Notation "a '-' b" :=  (UBinop (UBits2 UMinus) a b) (in custom koika at level 85
 Notation "a '==' b" :=  (UBinop UEq a b) (in custom koika at level 79, format "a  '=='  b").
 Notation "a '>>' b" :=  (UBinop (UBits2 ULsr) a b) (in custom koika at level 79, format "a  '>>'  b").
 Notation "a '<<' b" :=  (UBinop (UBits2 ULsl) a b) (in custom koika at level 79, format "a  '<<'  b").
-Notation "a  '<'  b" := (UBinop (UBits2 (UCompare false cLt) a b)) (in custom koika at level 79).
-Notation "a  '<s'  b" := (UBinop (UBits2 (UCompare true cLt) a b)) (in custom koika at level 79).
-Notation "a  '<='  b" := (UBinop (UBits2 (UCompare false cLe) a b)) (in custom koika at level 79).
-Notation "a  '<s='  b" := (UBinop (UBits2 (UCompare true cLe) a b)) (in custom koika at level 79).
-Notation "a  '>'  b" := (UBinop (UBits2 (UCompare false cGt) a b)) (in custom koika at level 79).
-Notation "a  '>s'  b" := (UBinop (UBits2 (UCompare true cGt) a b)) (in custom koika at level 79).
-Notation "a  '>='  b" := (UBinop (UBits2 (UCompare false cGe) a b)) (in custom koika at level 79).
-Notation "a  '>s='  b" := (UBinop (UBits2 (UCompare true cGe) a b)) (in custom koika at level 79).
+Notation "a  '<'  b" := (UBinop (UBits2 (UCompare false cLt)) a b) (in custom koika at level 79).
+Notation "a  '<s'  b" := (UBinop (UBits2 (UCompare true cLt)) a b) (in custom koika at level 79).
+Notation "a  '<='  b" := (UBinop (UBits2 (UCompare false cLe)) a b) (in custom koika at level 79).
+Notation "a  '<s='  b" := (UBinop (UBits2 (UCompare true cLe)) a b) (in custom koika at level 79).
+Notation "a  '>'  b" := (UBinop (UBits2 (UCompare false cGt)) a b) (in custom koika at level 79).
+Notation "a  '>s'  b" := (UBinop (UBits2 (UCompare true cGt)) a b) (in custom koika at level 79).
+Notation "a  '>='  b" := (UBinop (UBits2 (UCompare false cGe)) a b) (in custom koika at level 79).
+Notation "a  '>s='  b" := (UBinop (UBits2 (UCompare true cGe)) a b) (in custom koika at level 79).
 Notation "'{' a ',' b '}'" := (UBinop (UBits2 UConcat) a b) (in custom koika, format "'{' a ',' b '}'").
 Notation "a '++' b" :=  (UBinop (UBits2 UConcat) a b) (in custom koika at level 80,  right associativity, format "a  '++'  b").
 Notation "a '[' b ']'" := (UBinop (UBits2 USel) a b) (in custom koika at level 75, format "'[' a [ b ] ']'").
