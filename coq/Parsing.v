ab942d1dd94b58505a71f985a505c058f79dbff4
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-11-07 01:01:27 -0500

    coq: Generalize pack notation to allow enums as well

--- coq/Parsing.v
+++ coq/Parsing.v
@@ -105,7 +105,7 @@ Notation "'!' a" := (UUnop (UBits1 UNot) a) (in custom koika at level 75, a cust
 Notation "a '||' b" :=  (UBinop (UBits2 UOr) a b) (in custom koika at level 85, format "a  '||'  b").
 Notation "'zeroExtend(' a ',' b ')'" :=  (UUnop (UBits1 (UZExtL b)) a) (in custom koika, b constr at level 0, format "'zeroExtend(' a ',' b ')'").
 Notation "'pack(' a ')'" :=  (UUnop (UConv UPack) a) (in custom koika, a custom koika).
-Notation "'unpack(' t ',' v ')'" :=  (UUnop (UConv (UUnpack (struct_t t))) v) (in custom koika, t constr at level 11, v custom koika).
+Notation "'unpack(' t ',' v ')'" :=  (UUnop (UConv (UUnpack t)) v) (in custom koika, t constr at level 11, v custom koika).
 Notation "a  '^'  b" :=  (UBinop (UBits2 UXor) a b) (in custom koika at level 85).
 Notation "a  '+'  b" :=  (UBinop (UBits2 UPlus) a b) (in custom koika at level 85).
 Notation "a  '-'  b" :=  (UBinop (UBits2 UMinus) a b) (in custom koika at level 85).
@@ -271,6 +271,6 @@ Module Type Tests.
   Definition test_31' : uaction reg_t :=
     {{
         let a := struct mem_req {| foo := upu[#(Bits.of_nat 3 0) :+ 2] ; bar := |32`d98| |} in
-        unpack(mem_req, pack(a))
+        unpack(struct_t mem_req, pack(a))
     }}.
 End Tests.
