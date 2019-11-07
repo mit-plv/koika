74e5a5c90dc2c7c32d5a02bd8c677e443d100ed2
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-11-07 07:34:10 -0500

    examples: Move regfile example out of Demo.v and rename it to vector.v

---
+++ examples/vector.v
@@ -0,0 +1,76 @@
+Require Import Koika.Frontend.
+
+Definition nregs := 16.
+Definition reg_sz := 32.
+
+Inductive reg_t := rIndex | rData (n: Vect.index nregs) | rOutput.
+Inductive rule_name_t := read_reg | incr_index.
+
+Notation index_sz := (log2 nregs).
+
+Definition R r :=
+  match r with
+  | rIndex => bits_t index_sz
+  | rData n => bits_t reg_sz
+  | rOutput => bits_t reg_sz
+  end.
+
+Definition r reg : R reg :=
+  match reg with
+  | rIndex => Bits.zero
+  | rData n => Bits.of_nat _ (index_to_nat n)
+  | rOutput => Bits.zero
+  end.
+
+(* This macro expands into a switch that reads the n-th register
+     in rData *)
+Definition read_vect n : uaction reg_t empty_ext_fn_t :=
+  UCompleteSwitch
+    index_sz (pred nregs) n
+    (vect_map (fun idx => {{ read0(rData idx) }}) (all_indices nregs)).
+
+Definition _read_reg : uaction reg_t empty_ext_fn_t :=
+  {{
+      let v := read0(rIndex) in
+      write0(rOutput, `read_vect "v"`)
+  }}.
+
+Definition _incr_index : uaction reg_t empty_ext_fn_t :=
+  {{ write0(rIndex, read0(rIndex) + |index_sz`d1|) }}.
+
+Definition rules :=
+  tc_rules R empty_Sigma
+           (fun rl => match rl with
+                   | read_reg => _read_reg
+                   | incr_index => _incr_index
+                   end).
+
+Definition regfile : scheduler :=
+  tc_scheduler (read_reg |> incr_index |> done).
+
+Definition circuits :=
+  compile_scheduler rules regfile.
+
+Definition package :=
+  {| ip_koika := {| koika_reg_names := show;
+                   koika_reg_types := R;
+                   koika_reg_init reg := r reg;
+
+                   koika_ext_fn_types := empty_Sigma;
+
+                   koika_rules := rules;
+                   koika_rule_names := show;
+
+                   koika_scheduler := regfile;
+
+                   koika_module_name := "vector" |};
+
+     ip_sim := {| sp_var_names x := x;
+                 sp_ext_fn_names := empty_fn_names;
+                 sp_extfuns := None |};
+
+     ip_verilog := {| vp_external_rules := List.nil;
+                     vp_ext_fn_names := empty_fn_names |} |}.
+
+Definition prog := Interop.Backends.register package.
+Extraction "vector.ml" prog.
