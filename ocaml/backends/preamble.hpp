210b35ddcd83fc80c7317467b0754ac19f3680e4
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-11-13 21:11:08 -0500

    sim: Fix off-by-one error in hexadecimal literal parsing
    Co-authored-by: @bthom

--- ocaml/backends/preamble.hpp
+++ ocaml/backends/preamble.hpp
@@ -192,9 +192,9 @@ namespace prims {
       if ('0' <= c && c <= '9') {
         return c - '0';
       } else if ('a' <= c && c <= 'f') {
-        return c - 'a' + 9;
+        return c - 'a' + 10;
       } else if ('A' <= c && c <= 'F') {
-        return c - 'A' + 9;
+        return c - 'A' + 10;
       }
     }
 
