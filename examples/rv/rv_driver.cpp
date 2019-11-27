993da1f75c1ed1f93fa7d6c3d37e8958ec32d1b1
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-11-27 13:59:43 -0500

    rv: Use generic toplevel to simplify rv_driver.cpp

--- examples/rv/rv_driver.cpp
+++ examples/rv/rv_driver.cpp
@@ -1,91 +1,21 @@
-#include "rv32i_core_pipelined.v.objects/rv32i_core_pipelined.hpp"
-#include "elf.h"
-#include <fstream>
 #include <iostream>
-#define DMEM_SIZE (1 << 30)
 
-class extfuns {
-public:
-  /* External methods (if any) should be implemented here. */
-};
+#include "rv32i_core_pipelined.v.objects/rv32i_core_pipelined.hpp"
+#include "elf.hpp"
 
+#define DMEM_SIZE (static_cast<std::size_t>(1) << 30)
+
+class extfuns {};
 using sim_t = module_rv32i_core_pipelined<extfuns>;
 
-static bits<32>* dmem = new bits<32>[DMEM_SIZE];
-
 class rv_core : public sim_t {
+  std::unique_ptr<bits<32>[]> dmem;
 
 public:
-  explicit rv_core() : sim_t{} {}
-  void init_mem(char* elf_filename) {
-    std::ifstream elf_file;
-    char* elf_data;
-    unsigned long long elf_size;
-
-    elf_file.open(elf_filename, std::ios::in | std::ios::binary);
-
-    if (!elf_file) {
-	std::cerr << "ERROR: fail reading elf file" << std::endl;
-	exit(1);
-    }
-
-    // Get size of the elf
-    elf_file.seekg(0, elf_file.end);
-    elf_size = elf_file.tellg();
-    elf_file.seekg(0, elf_file.beg);
-
-    if (!elf_file) {
-	std::cerr << "ERROR: fail reading elf file" << std::endl;
-	exit(1);
-    }
-
-    if (elf_size < sizeof(Elf32_Ehdr)) {
-	std::cerr << "ERROR: the file is too small to be a valid elf" << std::endl;
-	exit(1);
-    }
-
-    elf_data = new char[elf_size];
-    elf_file.read(elf_data, elf_size);
-
-    // Check header
-    Elf32_Ehdr *ehdr = (Elf32_Ehdr *) elf_data;
-    unsigned char* e_ident = ehdr->e_ident;
-    if (e_ident[EI_MAG0] != ELFMAG0
-	    || e_ident[EI_MAG1] != ELFMAG1
-	    || e_ident[EI_MAG2] != ELFMAG2
-	    || e_ident[EI_MAG3] != ELFMAG3) {
-	std::cerr << "ERROR: the file is not a valid elf file" << std::endl;
-	exit(1);
-    }
-
-    if (e_ident[EI_CLASS] == ELFCLASS32) {
-	// 32-bit ELF, only case supported
-	Elf32_Ehdr *ehdr = (Elf32_Ehdr*) elf_data;
-	Elf32_Phdr *phdr = (Elf32_Phdr*) (elf_data + ehdr->e_phoff);
-	if (elf_size < ehdr->e_phoff + ehdr->e_phnum * sizeof(Elf32_Phdr)) {
-	    std::cerr << "ERROR: file too small for expected number of program header tables" << std::endl;
-	    exit(1);
-	}
-	// loop through program header tables
-	for (int i = 0 ; i < ehdr->e_phnum ; i++) {
-	    // only look at non-zero length PT_LOAD sections
-	    printf("Section of type: %d of size %d filesize %d starting at %x\n", phdr[i].p_type, phdr[i].p_memsz, phdr[i].p_filesz,phdr[i].p_paddr);
-	    if ((phdr[i].p_type == PT_LOAD) && (phdr[i].p_memsz > 0)) {
-		if (phdr[i].p_memsz < phdr[i].p_filesz) {
-		    std::cerr << "ERROR: file size is larger than target memory size" << std::endl;
-		    exit(1);
-		}
-		if ((phdr[i].p_filesz > 0) && (phdr[i].p_offset + phdr[i].p_filesz > elf_size)) {
-			std::cerr << "ERROR: file section overflow" << std::endl;
-		}
-		// Set the corresponding section in IMem
-		memcpy(&dmem[phdr[i].p_paddr>>2], elf_data + phdr[i].p_offset, phdr[i].p_filesz);
-	    }
-	}
-    } else {
-	 std::cerr << "ERROR: the file is not a 32-bit elf file" << std::endl;
-	 exit(1);
-    }
+  explicit rv_core(const std::string& elf_fpath)
+    // Use new … instead of make_unique to avoid 0-initialization
+    : sim_t{}, dmem(new bits<32>[DMEM_SIZE]) {
+    elf_load(reinterpret_cast<uint32_t*>(dmem.get()), elf_fpath.c_str());
   }
 
 protected:
@@ -108,21 +38,21 @@ protected:
           bits<4> IEn = readRequestI.byte_en;
           /* bind */ {
             struct_mem_resp _x3 =
-                prims::unpack<struct_mem_resp, 68>(0x68'000000000_x);
+              prims::unpack<struct_mem_resp, 68>(0x68'000000000_x);
             _x3.byte_en = IEn;
             struct_mem_resp _x2 = _x3;
             _x2.addr = IAddress;
             struct_mem_resp data = _x2;
-	    // Manually added
-	    bits<32> current_value = dmem[IAddress.v >> 2];
-	    data.data = current_value;
+            // Manually added
+            bits<32> current_value = dmem[IAddress.v >> 2];
+            data.data = current_value;
             bits<1> _x6;
             CHECK_RETURN(log.fromIMem_valid0.read0(&_x6, Log.fromIMem_valid0));
             if (bool(~(_x6))) {
               CHECK_RETURN(
-                  log.fromIMem_data0.write0(data, Log.fromIMem_data0));
+                           log.fromIMem_data0.write0(data, Log.fromIMem_data0));
               CHECK_RETURN(
-                  log.fromIMem_valid0.write0(1'1_b, Log.fromIMem_valid0));
+                           log.fromIMem_valid0.write0(1'1_b, Log.fromIMem_valid0));
             } else {
               return false;
             }
@@ -135,7 +65,7 @@ protected:
     return true;
   }
 
- virtual bool rule_ExternalD() {
+  virtual bool rule_ExternalD() {
     reset_ExternalD();
 
     /* bind */ {
@@ -154,41 +84,41 @@ protected:
           bits<4> DEn = readRequestD.byte_en;
           /* bind */ {
             struct_mem_resp _x3 =
-                prims::unpack<struct_mem_resp, 68>(0x68'000000000_x);
+              prims::unpack<struct_mem_resp, 68>(0x68'000000000_x);
             _x3.byte_en = DEn;
             struct_mem_resp _x2 = _x3;
             _x2.addr = DAddress;
             struct_mem_resp data = _x2;
-	    	    // Manually added
-	    bits<32> current_value = dmem[DAddress.v >> 2];
-	    if (DAddress.v == 0x40000000 && DEn.v == 0xf) { // PutChar  && DEn.v == 0xf
-	      printf("%c", (char) readRequestD.data.v);
-	    }
-	    if (DAddress.v == 0x40001000 && DEn.v == 0xf) {
-	      exit(readRequestD.data.v);
-	    }
-	   // if (DAddress.v == 0xffff4 && DEn.v == 0) { // GetChar
-	   //   scanf("%c", (char *) &data.data.v);
-	   // }
-	    else {
-	      data.data = current_value;
-	    }
+            // Manually added
+            bits<32> current_value = dmem[DAddress.v >> 2];
+            if (DAddress.v == 0x40000000 && DEn.v == 0xf) { // PutChar  && DEn.v == 0xf
+              printf("%c", (char) readRequestD.data.v);
+            }
+            if (DAddress.v == 0x40001000 && DEn.v == 0xf) {
+              exit(readRequestD.data.v);
+            }
+            // if (DAddress.v == 0xffff4 && DEn.v == 0) { // GetChar
+            //   scanf("%c", (char *) &data.data.v);
+            // }
+            else {
+              data.data = current_value;
+            }
 
-	    bits<32> mask0 = 0x32'000000ff_x;
-	    bits<32> mask1 = 0x32'0000ff00_x;
-	    bits<32> mask2 = 0x32'00ff0000_x;
-	    bits<32> mask3 = 0x32'ff000000_x;
-	    dmem[DAddress.v >> 2] = (DEn[2'0_d].v == 1 ? (readRequestD.data & mask0) : (current_value & mask0))
-	      | (DEn[2'1_d].v == 1 ? (readRequestD.data & mask1) : (current_value & mask1))
-	      | (DEn[2'2_d].v == 1 ? (readRequestD.data & mask2) : (current_value & mask2))
-	      | (DEn[2'3_d].v == 1 ? (readRequestD.data & mask3) : (current_value & mask3));
+            bits<32> mask0 = 0x32'000000ff_x;
+            bits<32> mask1 = 0x32'0000ff00_x;
+            bits<32> mask2 = 0x32'00ff0000_x;
+            bits<32> mask3 = 0x32'ff000000_x;
+            dmem[DAddress.v >> 2] = (DEn[2'0_d].v == 1 ? (readRequestD.data & mask0) : (current_value & mask0))
+              | (DEn[2'1_d].v == 1 ? (readRequestD.data & mask1) : (current_value & mask1))
+              | (DEn[2'2_d].v == 1 ? (readRequestD.data & mask2) : (current_value & mask2))
+              | (DEn[2'3_d].v == 1 ? (readRequestD.data & mask3) : (current_value & mask3));
             bits<1> _x6;
             CHECK_RETURN(log.fromDMem_valid0.read0(&_x6, Log.fromDMem_valid0));
             if (bool(~(_x6))) {
               CHECK_RETURN(
-                  log.fromDMem_data0.write0(data, Log.fromDMem_data0));
+                           log.fromDMem_data0.write0(data, Log.fromDMem_data0));
               CHECK_RETURN(
-                  log.fromDMem_valid0.write0(1'1_b, Log.fromDMem_valid0));
+                           log.fromDMem_valid0.write0(1'1_b, Log.fromDMem_valid0));
             } else {
               return false;
             }
@@ -203,29 +133,16 @@ protected:
 
 };
 
-
-sim_t::state_t init_and_run(char* filename, unsigned long long int ncycles) {
-  rv_core simulator{};
-  simulator.init_mem(filename);
-  simulator.run(ncycles);
-  return simulator.snapshot();
-}
-
-
 #ifndef SIM_MINIMAL
 int main(int argc, char **argv) {
-  unsigned long long int ncycles = 1000;
-  char* filename;
-  if (argc >= 3) {
-    ncycles = std::stoull(argv[2]);
-    filename = argv[1];
-  } else {
-	std::cerr << "Argument missing.\n\nUsage: ./rv_core elf_file number_cycles" << std::endl;
-	exit(1);
+  if (argc <= 1) {
+    std::cerr << "Usage: ./rv_core elf_file [number_cycles [vcd_path [vcd_period]]]" << std::endl;
+    return 1;
   }
 
-  sim_t::state_t snapshot = init_and_run(filename, ncycles);
-  snapshot.dump();
-  return 0;
+  setbuf(stdout, NULL);
+  std::ios_base::sync_with_stdio(false);
+  cuttlesim::toplevel<rv_core> top(argv[1]);
+  return top.main(argc - 1, argv + 1);
 }
 #endif
