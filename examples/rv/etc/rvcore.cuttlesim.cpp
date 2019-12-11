62110c770b5fd59bc7c52231d811b7da73654219
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-12-11 10:47:15 -0500

    rv: Rewrite the C++ model of the BRAM to answer with a one-cycle delay

--- examples/rv/etc/rvcore.cuttlesim.cpp
+++ examples/rv/etc/rvcore.cuttlesim.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <optional>
 
-// #define SIM_FLATTEN
 #include "rv32i_core_pipelined.hpp"
 #include "elf.hpp"
 
@@ -19,104 +19,72 @@ public:
   }
 
 protected:
+
+#define PEEK(rn) log.state.rn
+
   virtual bool rule_ExternalI() noexcept {
-    {
-      bits<1> _c0;
-      READ1_FAST(ExternalI, toIMem_valid0, &_c0);
-      if (_c0) {
-        WRITE1_FAST(ExternalI, toIMem_valid0, 1'0_b);
-      } else {
-        FAIL_FAST(ExternalI);
-      }
-      struct_mem_req readRequestI;
-      READ1_FAST(ExternalI, toIMem_data0, &readRequestI);
-      {
-        bits<32> IAddress = readRequestI.addr;
-        {
-          bits<4> IEn = readRequestI.byte_en;
-          {
-            struct_mem_resp data = struct_mem_resp{};
-            data.byte_en = IEn;
-            data.addr = IAddress;
-            // -- Manually added --
-            bits<32> current_value = dmem[IAddress.v >> 2];
-            data.data = current_value;
-            // --------------------
-            bits<1> _x2;
-            READ1_FAST(ExternalI, fromIMem_valid0, &_x2);
-            if (~(_x2)) {
-              WRITE1_FAST(ExternalI, fromIMem_data0, data);
-              WRITE1_FAST(ExternalI, fromIMem_valid0, 1'1_b);
-            } else {
-              FAIL(ExternalI);
-            }
-          }
-        }
-      }
+    static std::optional<struct_mem_req> last{};
+
+    if (~PEEK(fromIMem_valid0) && last.has_value()) {
+      PEEK(fromIMem_valid0) = 1'1_b;
+      PEEK(fromIMem_data0) = struct_mem_resp {
+        .byte_en = last->byte_en,
+        .addr = last->addr,
+        .data = dmem[last->addr.v >> 2]
+      };
+      last.reset();
     }
 
-    commit_ExternalI();
+    if (PEEK(toIMem_valid0) && !last.has_value()) {
+      PEEK(toIMem_valid0) = 1'0_b;
+      last = PEEK(toIMem_data0);
+    }
+
+    COMMIT(ExternalI);
     return true;
   }
 
   virtual bool rule_ExternalD() noexcept {
-    {
-      bits<1> _c0;
-      READ1_FAST(ExternalD, toDMem_valid0, &_c0);
-      if (_c0) {
-        WRITE1_FAST(ExternalD, toDMem_valid0, 1'0_b);
-      } else {
-        FAIL_FAST(ExternalD);
+    static std::optional<struct_mem_req> last{};
+
+    if (!PEEK(fromDMem_valid0) && last.has_value()) {
+      PEEK(fromDMem_valid0) = 1'1_b;
+
+      auto data = last->data;
+      auto addr = last->addr;
+      auto dEn = last->byte_en;
+      auto current = dmem[addr.v >> 2];
+
+      PEEK(fromDMem_data0) = struct_mem_resp {
+        .byte_en = last->byte_en,
+        .addr = last->addr,
+        .data = current
+      };
+
+      if (addr.v == 0x40000000 && dEn.v == 0xf) { // PutChar
+        putchar(static_cast<char>(last->data.v));
+      } else if (addr.v == 0x40001000 && dEn.v == 0xf) {
+        std::exit(last->data.v);
+      } // else if (addr == 0xffff4 && dEn.v == 0) { // GetChar
+      //   data.data.v = getchar();
+      // }
+      else {
+        dmem[addr.v >> 2] =
+          ((dEn[2'0_d] ? data : current) & 0x32'000000ff_x) |
+          ((dEn[2'1_d] ? data : current) & 0x32'0000ff00_x) |
+          ((dEn[2'2_d] ? data : current) & 0x32'00ff0000_x) |
+          ((dEn[2'3_d] ? data : current) & 0x32'ff000000_x);
       }
-      struct_mem_req readRequestD;
-      READ1_FAST(ExternalD, toDMem_data0, &readRequestD);
-      {
-        bits<32> DAddress = readRequestD.addr;
-        {
-          bits<4> DEn = readRequestD.byte_en;
-          {
-            struct_mem_resp data = struct_mem_resp{};
-            data.byte_en = DEn;
-            data.addr = DAddress;
 
-            // -- Manually added --
-            bits<32> current_value = dmem[DAddress.v >> 2];
-            if (DAddress.v == 0x40000000 && DEn.v == 0xf) { // PutChar  && DEn.v == 0xf
-              putchar((char)readRequestD.data.v);
-            } else if (DAddress.v == 0x40001000 && DEn.v == 0xf) {
-              std::exit(readRequestD.data.v);
-            }
-            // else if (DAddress.v == 0xffff4 && DEn.v == 0) { // GetChar
-            //   data.data.v = getchar();
-            // }
-            else {
-              data.data = current_value;
-            }
-
-            bits<32> mask0 = 0x32'000000ff_x;
-            bits<32> mask1 = 0x32'0000ff00_x;
-            bits<32> mask2 = 0x32'00ff0000_x;
-            bits<32> mask3 = 0x32'ff000000_x;
-            dmem[DAddress.v >> 2] = (DEn[2'0_d].v == 1 ? (readRequestD.data & mask0) : (current_value & mask0))
-              | (DEn[2'1_d].v == 1 ? (readRequestD.data & mask1) : (current_value & mask1))
-              | (DEn[2'2_d].v == 1 ? (readRequestD.data & mask2) : (current_value & mask2))
-              | (DEn[2'3_d].v == 1 ? (readRequestD.data & mask3) : (current_value & mask3));
-            // --------------------
-
-            bits<1> _x2;
-            READ1_FAST(ExternalD, fromDMem_valid0, &_x2);
-            if (~(_x2)) {
-              WRITE1_FAST(ExternalD, fromDMem_data0, data);
-              WRITE1_FAST(ExternalD, fromDMem_valid0, 1'1_b);
-            } else {
-              FAIL(ExternalD);
-            }
-          }
-        }
-      }
+      last.reset();
     }
 
-    commit_ExternalD();
+    if (PEEK(toDMem_valid0) && !last.has_value()) {
+      PEEK(toDMem_valid0) = 1'0_b;
+      last = PEEK(toDMem_data0);
+    }
+
+    COMMIT(ExternalD);
     return true;
   }
 
@@ -139,4 +107,5 @@ int main(int argc, char** argv) {
 
 // Local Variables:
 // flycheck-clang-include-path: ("../_objects/rv32i_core_pipelined.v/")
+// flycheck-clang-language-standard: "c++17"
 // End:
