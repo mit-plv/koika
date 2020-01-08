e8ba03a34819cddcebe2e345531448aa217c2979
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-01-08 16:35:16 -0500

    tests: Add an extra test in muxelim, which isn't properly optimized yet

--- tests/muxelim.v
+++ tests/muxelim.v
@@ -1,56 +1,77 @@
 (*! Sanity check for mux-elimination optimization !*)
 Require Import Koika.Frontend.
 
-Inductive cnt := C0 | C1 | C2 | C3 | C4 | C5 | C6.
-Inductive reg_t := rc1 (v: cnt) | rc3 | rdata1 | rdata3.
-Inductive rule_name_t := rl1 | rl3.
+Inductive nested_cnt :=
+  NC0 | NC1 | NC2 | NC3 | NC4 | NC5 | NC6.
+Inductive redundant_cnt :=
+  RC0 | RC1.
+Inductive reg_t :=
+| rc_nested (v: nested_cnt)
+| rc_redundant (c: redundant_cnt)
+| rc_bitwise
+| rdata_nested
+| rdata_redundant
+| rdata_bitwise.
+Inductive rule_name_t := rl_nested | rl_redundant | rl_bitwise.
 
 Definition R reg : type :=
   match reg with
-  | rc1 _ => bits_t 1
-  | rc3 => bits_t 3
-  | rdata1 => bits_t 8
-  | rdata3 => bits_t 8
+  | rc_nested _ => bits_t 1
+  | rc_redundant _ => bits_t 1
+  | rc_bitwise => bits_t 3
+  | rdata_nested => bits_t 8
+  | rdata_redundant => bits_t 8
+  | rdata_bitwise => bits_t 8
   end.
 
 Definition r reg : R reg :=
   match reg with
-  | rc1 _ => Bits.zero
-  | rc3 => Bits.zero
-  | rdata1 => Bits.zero
-  | rdata3 => Bits.zero
+  | rc_nested _ => Bits.zero
+  | rc_redundant _ => Bits.zero
+  | rc_bitwise => Bits.zero
+  | rdata_nested => Bits.zero
+  | rdata_redundant => Bits.zero
+  | rdata_bitwise => Bits.zero
   end.
 
 Definition urules rl : uaction reg_t empty_ext_fn_t :=
   match rl with
-  | rl1 => {{ if read0(rc1 C0) then
-               if read0(rc1 C1) then
-                 if read0(rc1 C2) then pass
-                 else if read0(rc1 C3) then
-                        if read0(rc1 C4) then pass
-                        else if read0(rc1 C5) then
-                               if read0(rc1 C6) then
-                                 write0(rdata1, |8`d42|)
-                               else pass
-                             else pass
-                      else pass
-               else pass
-             else pass }}
-  | rl3 => {{ match read0(rc3) with
-             | Ob~0~1~0 => write0(rdata3, |8`d0|)
-             | Ob~1~0~0 => write0(rdata3, |8`d1|)
-             | Ob~1~1~0 => write0(rdata3, |8`d2|)
-             | Ob~1~1~1 => write0(rdata3, |8`d3|)
-             | Ob~0~0~0 => write0(rdata3, |8`d4|)
-             return default: fail
-             end }}
+  | rl_nested =>
+    {{ if read0(rc_nested NC0) then
+         if read0(rc_nested NC1) then
+           if read0(rc_nested NC2) then pass
+           else if read0(rc_nested NC3) then
+                  if read0(rc_nested NC4) then pass
+                  else if read0(rc_nested NC5) then
+                         if read0(rc_nested NC6) then
+                           write0(rdata_nested, |8`d24|)
+                         else pass
+                       else pass
+                else pass
+         else pass
+       else pass }}
+  | rl_redundant =>
+    {{ if read0(rc_redundant RC0) then
+         if read0(rc_redundant RC1) then
+           write1(rdata_redundant, |8`d42|)
+         else pass
+       else pass }}
+  | rl_bitwise =>
+    {{ match read0(rc_bitwise) with
+       | Ob~0~1~0 => write0(rdata_bitwise, |8`d0|)
+       | Ob~1~0~0 => write0(rdata_bitwise, |8`d1|)
+       | Ob~1~1~0 => write0(rdata_bitwise, |8`d2|)
+       | Ob~1~1~1 => write0(rdata_bitwise, |8`d3|)
+       | Ob~0~0~0 => write0(rdata_bitwise, |8`d4|)
+       return default: fail
+       end }}
   end.
 
 Definition rules :=
   tc_rules R empty_Sigma urules.
 
 Definition sched : scheduler :=
-  tc_scheduler (rl1 |> rl3 |> done).
+  tc_scheduler (rl_nested |> rl_redundant |> rl_bitwise |> done).
 
 Definition sched_result :=
   tc_compute (interp_scheduler (ContextEnv.(create) r) empty_sigma rules sched).
