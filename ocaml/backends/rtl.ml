ed833bb8e8b08500c59d5dd634f253b08d1e8674
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-02-21 13:28:41 -0500

    backends/rtl: Separate expressions and modules

--- ocaml/backends/rtl.ml
+++ ocaml/backends/rtl.ml
@@ -23,14 +23,16 @@ type expr =
   | EUnop of Extr.PrimTyped.fbits1 * node
   | EBinop of Extr.PrimTyped.fbits2 * node * node
   | EMux of node * node * node
-  | EModule of { name: string;
-                 internal: bool;
-                 inputs: input_pin list;
-                 outputs: output_pin list }
+and entity =
+  | Expr of expr
+  | Module of { name: string;
+                internal: bool;
+                inputs: input_pin list;
+                outputs: output_pin list }
 and node =
   { tag: int;
     size: int;
-    expr: expr;
+    entity: entity;
     annots: string list;
     mutable kind: node_kind }
 and node_kind =
@@ -46,7 +48,7 @@ type node_map = (int, node) Hashtbl.t
 type pin_set = (string, [`Input | `Output] * int) Hashtbl.t
 
 let may_share = function
-  | EUnop _ | EBinop _ | EMux _ -> true
+  | Module _ | Expr (EUnop _ | EBinop _ | EMux _) -> true
   | _ -> false
 
 let fn1_sz fn =
@@ -112,31 +114,31 @@ let field_name instance field =
   sprintf "%s_%s" instance field
 
 let node_of_circuit (metadata: metadata_map) (cache: node_map) (ios: pin_set) (c: circuit) =
-  let fresh_node size expr =
-    { tag = -1; size; annots = []; expr; kind = Unique } in
+  let fresh_node size entity =
+    { tag = -1; size; annots = []; entity; kind = Unique } in
   let rec lift ({ tag; node = c'; _ }: circuit) =
     match Hashtbl.find_opt cache tag with
     | Some node -> node
     | None ->
-       let size, annots, expr = lift' c' in
-       let n = { tag; size; expr; annots; kind = Unique } in
+       let size, annots, entity = lift' c' in
+       let n = { tag; size; entity; annots; kind = Unique } in
        let shared = (Hashtbl.find metadata tag).shared in
-       if shared && may_share expr then n.kind <- Shared;
+       if shared && may_share entity then n.kind <- Shared;
        Hashtbl.replace cache n.tag n;
        n
   and lift' (c: circuit') =
     match c with
-    | CMux (sz, s, c1, c2) -> sz, [], EMux (lift s, lift c1, lift c2)
-    | CConst c -> Array.length c, [], EConst c
-    | CReadRegister r -> typ_sz (reg_type r), [], EName (r.reg_name)
-    | CUnop (f, c1) -> fn1_sz f, [], EUnop (f, lift c1)
-    | CBinop (f, c1, c2) -> fn2_sz f, [], EBinop (f, lift c1, lift c2)
+    | CMux (sz, s, c1, c2) -> sz, [], Expr (EMux (lift s, lift c1, lift c2))
+    | CConst c -> Array.length c, [], Expr (EConst c)
+    | CReadRegister r -> typ_sz (reg_type r), [], Expr (EName (r.reg_name))
+    | CUnop (f, c1) -> fn1_sz f, [], Expr (EUnop (f, lift c1))
+    | CBinop (f, c1, c2) -> fn2_sz f, [], Expr (EBinop (f, lift c1, lift c2))
     | CExternal (f, c) ->
        let size = typ_sz f.ffi_rettype in
        let inputs = [{ ip_name = "arg"; ip_node = lift c }] in
        let outputs = [{ op_name = "out"; op_wire = "out"; op_sz = typ_sz f.ffi_rettype }] in
-       let mod_expr = EModule { name = f.ffi_name; internal = true; inputs; outputs } in
-       size, [], EPtr (fresh_node size mod_expr, Some "out")
+       let mod_ent = Module { name = f.ffi_name; internal = true; inputs; outputs } in
+       size, [], Expr (EPtr (fresh_node size mod_ent, Some "out"))
     | CBundle (name, bundle_ios) ->
        let name = "rule_" ^ name in
        let reg_ios = List.map inputs_outputs_of_module_io bundle_ios in
@@ -146,14 +148,14 @@ let node_of_circuit (metadata: metadata_map) (cache: node_map) (ios: pin_set) (c
        (* The module's outputs are our inputs, and vice-versa *)
        List.iter (fun op -> Hashtbl.replace ios (field_name name op.op_name) (`Input, op.op_sz)) outputs;
        List.iter (fun ip -> Hashtbl.replace ios (field_name name ip.ip_name) (`Output, ip.ip_node.size)) inputs;
-       0, [], EModule { name; internal = compile_bundles_internally; inputs; outputs }
+       0, [], Module { name; internal = compile_bundles_internally; inputs; outputs }
     | CBundleRef (sz, c, field) ->
-       sz, [], EPtr (lift c, Some (field_name after_prefix (rwcircuit_to_string field)))
+       sz, [], Expr (EPtr (lift c, Some (field_name after_prefix (rwcircuit_to_string field))))
     | CAnnot (sz, annot, c) ->
        let ann = if annot = "" then [] else [annot] in
        match lift c with
-       | { kind = Unique; annots; expr; _ } -> sz, ann @ annots, expr
-       | n -> sz, ann, EPtr (n, None)
+       | { kind = Unique; annots; entity; _ } -> sz, ann @ annots, entity
+       | n -> sz, ann, Expr (EPtr (n, None))
   and inputs_outputs_of_module_io (reg, rwd) =
     List.map (fun (field, c) ->
         let ip_node = lift c in
@@ -201,8 +203,11 @@ module Debug = struct
     | EMux (s, c1, c2) -> sprintf "EMux (%d, %d, %d)" s.tag c1.tag c2.tag
     | EUnop (f, c) -> sprintf "EUnop (%s, %d)" (unop_to_str f) c.tag
     | EBinop (f, c1, c2) -> sprintf "EBinop (%s, %d, %d)" (binop_to_str f) c1.tag c2.tag
-    | EModule { name; internal; inputs; outputs } ->
-       sprintf "EModule (%s, internal=%b, [%s], [%s])" name internal
+
+  let entity_to_str = function
+    | Expr e -> expr_to_str e
+    | Module { name; internal; inputs; outputs } ->
+       sprintf "Module (%s, internal=%b, [%s], [%s])" name internal
          (String.concat "; "
             (List.map (fun { ip_name; ip_node } ->
                  sprintf "(%s, %d)" ip_name ip_node.tag) inputs))
@@ -215,9 +220,9 @@ module Debug = struct
     | Shared -> "Shared"
     | Printed { name } -> sprintf "Printed { name = \"%s\" }" name
 
-  let node_to_str { tag; expr; annots; kind; _ } =
-    sprintf "{ tag=%d; kind=%s; expr=%s; annots=[%s] }"
-      tag (node_kind_to_str kind) (expr_to_str expr) (String.concat "; " annots)
+  let node_to_str { tag; entity; annots; kind; _ } =
+    sprintf "{ tag=%d; kind=%s; entity=%s; annots=[%s] }"
+      tag (node_kind_to_str kind) (entity_to_str entity) (String.concat "; " annots)
 
   let circuit_to_str =
     let open Cuttlebone.Graphs in
@@ -249,9 +254,9 @@ module Debug = struct
             tag shared (circuit_to_str circuit))
         metadata
 
-  let annotate s n =
+  let annotate annots s =
     if add_debug_annotations then
-      match n.annots with
+      match annots with
       | [] -> s
       | ans -> sprintf "/*<%s>*/%s" (String.concat "; " ans) s
     else s
@@ -309,7 +314,7 @@ module Verilog : RTLBackend = struct
     | Some expr -> p_stmt out 1 "%s %s = %s" typ name expr
 
   let precedence = function
-    | EName _ | EConst _ | EModule _ -> -1
+    | EName _ | EConst _ -> -1
     | EPtr _ -> -2
     | EUnop (Slice _, _) | EBinop ((Sel _ | IndexedSlice _), _, _) -> -3
     | EUnop (Not _, _) -> -4
@@ -326,30 +331,27 @@ module Verilog : RTLBackend = struct
     | EUnop ((SExt _ | ZExtL _ | ZExtR _ | Lowered _), _)
       | EBinop (SliceSubst _, _, _) -> unlowered ()
 
-  let complex_op = function
-    | { expr = (EPtr _ | EName _); _ } -> false
+  let complex_expr = function
+    | EPtr _ | EName _ -> false
     (* Yosys doesn't support chained selects (x[4 +: 32][3 +: 2]) *)
-    | { expr = (EUnop (Slice _, _) | EBinop ((Sel _ | IndexedSlice _), _, _)); _ } -> true
-    | { expr = (EUnop _ | EBinop _ | EMux _ | EConst _); _ } -> true
-    | { expr = EModule _; _ } -> assert false
+    | EUnop (Slice _, _) | EBinop ((Sel _ | IndexedSlice _), _, _) -> true
+    | EUnop _ | EBinop _ | EMux _ | EConst _ -> true
 
   let sp_cast signed p () x =
     sprintf (if signed then "$signed(%a)" else "$unsigned(%a)") p x
 
-  type result = [`Expr | `Name] * int * string
-
-  let rec p_expr out (e: expr) : result =
+  let rec p_expr out (e: expr) =
     let lvl = precedence e in
     let sp_str () s = s in
     let p () n = snd (p_node out lvl n) in
     let pr () n = snd (p_node ~restricted_ctx:true out min_int n) in
     let p0 () n = snd (p_node out min_int n) in
-    let sp fmt = ksprintf (fun s -> (`Expr, lvl, s)) fmt in
+    let sp fmt = ksprintf (fun s -> (lvl, s)) fmt in
     match e with
-    | EPtr (n, None) -> (`Expr, lvl, p () n)
-    | EPtr (n, Some field) -> let nm = p () n in (`Expr, lvl, field_name nm field)
-    | EName name -> (`Expr, lvl, name)
-    | EConst cst -> (`Expr, lvl, string_of_bits cst)
+    | EPtr (n, None) -> (lvl, p () n)
+    | EPtr (n, Some field) -> let nm = p () n in (lvl, field_name nm field)
+    | EName name -> (lvl, name)
+    | EConst cst -> (lvl, string_of_bits cst)
     | EUnop (f, e) ->
        (match f with
         | Not _ -> sp "~%a" p e
@@ -385,7 +387,10 @@ module Verilog : RTLBackend = struct
         | Concat (_, _) -> sp "{%a, %a}" p0 e1 p0 e2
         | SliceSubst _ -> unlowered ())
     | EMux (s, e1, e2) -> sp "%a ? %a : %a" p s p e1 p e2
-    | EModule { name; internal = true; inputs; outputs } ->
+  and p_shared_entity out n =
+    let p0 () n = snd (p_node out min_int n) in
+    match n.entity with
+    | Module { name; internal = true; inputs; outputs } ->
        let instance_name = gensym_next ("mod_" ^ name) in
        let outputs = List.map (fun op -> { op with op_wire = field_name instance_name op.op_wire }) outputs in
        let in_args = List.map (fun { ip_name; ip_node } -> sprintf ".%s(%a)" ip_name p0 ip_node) inputs in
@@ -393,30 +398,34 @@ module Verilog : RTLBackend = struct
        let args = String.concat ", " (".CLK(CLK)" :: in_args @ out_args) in
        List.iter (fun { op_wire; op_sz; _ } -> p_decl out "wire" op_sz op_wire) outputs;
        p_stmt out 1 "%s %s(%s)" name instance_name args;
-       (`Name, lvl, instance_name)
-    | EModule { name; internal = false; inputs; _ } ->
-       List.iter (fun { ip_name; ip_node } -> p_assign out (field_name name ip_name) (p0 () ip_node)) inputs;
-       (`Name, lvl, name)
-  and p_wrapped_expr out ctx_lvl n =
-    let kd, lvl, s = p_expr out n.expr in
-    if kd = `Name then n.kind <- Printed { name = s };
-    let s = Debug.annotate s n in
+       instance_name
+    | Module { name; internal = false; inputs; _ } ->
+       List.iter (fun { ip_name; ip_node } ->
+           p_assign out (field_name name ip_name) (p0 () ip_node))
+         inputs;
+       name
+    | Expr e ->
+       let _, s = p_wrapped_expr out min_int n.annots e in
+       let name = name_node n in
+       p_decl out "wire" n.size name ~expr:s;
+       name
+  and p_wrapped_expr out ctx_lvl annots expr =
+    let lvl, s = p_expr out expr in
+    let s = Debug.annotate annots s in
     if lvl <= ctx_lvl then (0, sprintf "(%s)" s) else (lvl, s)
-  and p_shared_node out n =
-    let _, s = p_wrapped_expr out min_int n in
-    match n.kind with (* Check if p_wrapped_expr printed the node (that's the module case) *)
-    | Printed { name } -> 0, name
-    | _ -> let name = name_node n in
-           p_decl out "wire" n.size name ~expr:s;
-           n.kind <- Printed { name };
-           (0, name)
+  and p_shared_node out (n: node) =
+    let name = p_shared_entity out n in
+    n.kind <- Printed { name };
+    (0, name)
   and p_node ?(restricted_ctx=false) out ctx_lvl (n: node) =
     match n.kind with
     | Shared -> p_shared_node out n
     | Printed { name } -> (0, name)
     | Unique ->
-       if restricted_ctx && complex_op n then p_shared_node out n
-       else p_wrapped_expr out ctx_lvl n
+       match n.entity with
+       | Module _ -> p_shared_node out n
+       | Expr e -> if restricted_ctx && complex_expr e then p_shared_node out n
+                   else p_wrapped_expr out ctx_lvl n.annots e
 
   let p_delayed out name expr =
     p_stmt out 3 "%s <= %s" name expr
