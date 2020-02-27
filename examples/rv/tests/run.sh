12cf30a431a1b80dd5bb1cd39af153475defa89f
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-02-27 13:25:44 -0500

    examples/rv: Add a target to run tests in pyverilator

--- examples/rv/tests/run.sh
+++ examples/rv/tests/run.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 sim=$1
-prog=$2
+binary=$2
+args=(${@:3})
 TIMEFORMAT=$'  real: %3lR\tuser: %3lU\tsys: %3lS'
 
-echo "- $(basename "$sim") $prog"
+echo "./$(basename "$sim") $(basename "$binary")"
 
-case $prog in
+case $binary in
     *.rv32)
-        time "$sim" "$prog" -1
+        echo "  $sim $binary" -1
+        time "$sim" "$binary" -1 "${args[@]}"
         case $? in
             0) echo "  [0;32mPASS[0m";;
             *) echo "  [0;31mFAIL[0m ($?)";;
         esac;;
     *.vmh) # top.v expects the memory in mem.vmh
-        sim_dir=$(dirname "$(realpath "$sim")")
-        ln -f -s "$(realpath "$prog")" "$sim_dir/mem.vmh";
-        # The verilator driver always returns 0
-        time (cd "$sim_dir" || exit 1; "./$(basename "$sim")" -1);;
+        sim_name=$(basename "$sim")
+        sim_dir=$(dirname "$(realpath --relative-to=. "$sim")")
+        vmh_path=$(realpath --relative-to="$sim_dir" "$binary")
+        echo "  ln -f -s $vmh_path mem.vmh && ./$sim_name -1 ${args[*]}"
+        cd "$sim_dir" && ln -f -s "$vmh_path" mem.vmh && time "./$sim_name" -1  "${args[@]}";;
 esac
-
