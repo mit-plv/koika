e69cfc8bf2fccd1bee02da7c936b95f4c90109a4
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-09-30 16:16:30 -0400

    examples: Sketch a multi-cycle proof for the pipeline example

--- examples/pipeline_tutorial.v
+++ examples/pipeline_tutorial.v
@@ -9,11 +9,13 @@ Notation rule := (rule pos_t var_t fn_name_t).
 
 Notation scheduler := (scheduler pos_t _).
 
-Notation UInternalFunction reg_t ext_fn_t := (InternalFunction var_t fn_name_t (uaction reg_t ext_fn_t)).
-Notation InternalFunction R Sigma sig tau := (InternalFunction var_t fn_name_t (action R Sigma sig tau)).
+Notation UInternalFunction reg_t ext_fn_t :=
+  (InternalFunction var_t fn_name_t (uaction reg_t ext_fn_t)).
+Notation InternalFunction R Sigma sig tau :=
+  (InternalFunction var_t fn_name_t (action R Sigma sig tau)).
 
-Inductive reg_t := queue_data | output_buffer | input_buffer | queue_invalid.
-Inductive ext_fn_t := Stream | F | G.
+Inductive reg_t := queue_data | output_buffer | input_buffer | queue_empty.
+Inductive ext_fn_t := NextInput | F | G.
 Inductive rule_name_t := doF | doG.
 
 Definition sz := (pow2 5).
@@ -23,20 +25,12 @@ Definition R r :=
   | queue_data => bits_t sz
   | input_buffer => bits_t sz
   | output_buffer => bits_t sz
-  | queue_invalid => bits_t 1
-  end.
-
-Definition r reg : R reg :=
-  match reg with
-  | queue_data => Bits.zero
-  | output_buffer => Bits.zero
-  | input_buffer => Bits.zero
-  | queue_invalid => Ob~1
+  | queue_empty => bits_t 1
   end.
 
 Definition Sigma (fn: ext_fn_t) : ExternalSignature :=
   match fn with
-  | Stream => {$ bits_t sz ~> bits_t sz $}
+  | NextInput => {$ bits_t sz ~> bits_t sz $}
   | F => {$ bits_t sz ~> bits_t sz $}
   | G => {$ bits_t sz ~> bits_t sz $}
   end.
@@ -44,22 +38,22 @@ Definition Sigma (fn: ext_fn_t) : ExternalSignature :=
 Definition _doF : uaction _ _ :=
   {{
      let v := read0(input_buffer) in
-     write0(input_buffer, extcall Stream(v));
-     let queue_invalid := read1(queue_invalid) in
-     if queue_invalid then
-       write1(queue_invalid, Ob~0);
-       write0(queue_data,extcall F(v))
+     write0(input_buffer, extcall NextInput(v));
+     let queue_empty := read1(queue_empty) in
+     if queue_empty then
+       write1(queue_empty, Ob~0);
+       write0(queue_data, extcall F(v))
      else
        fail
   }}.
 
 Definition _doG : uaction _ _ :=
   {{
-      let queue_invalid := read0(queue_invalid) in
-      if !queue_invalid then
+      let queue_empty := read0(queue_empty) in
+      if !queue_empty then
         let data := read0(queue_data) in
         write0(output_buffer, extcall G(data));
-        write0(queue_invalid, Ob~1)
+        write0(queue_empty, Ob~1)
       else
         fail
   }}.
@@ -82,7 +76,6 @@ Section correct.
 
   Instance FiniteType_reg_t : FiniteType reg_t := _.
 
-
   Lemma beq_dec_refl {A} {EQ: EqDec A}:
     forall a, beq_dec a a = true.
   Proof.
@@ -91,36 +84,39 @@ Section correct.
     rewrite eq_dec_refl; reflexivity.
   Qed.
 
-  (* FIXME: is it a cbn bug that adding exclamation points below prevents reduction?*)
-  (* Probably due to implicits *)
-  (* Arguments may_read _ _ _ /. *)
-  (* Arguments may_write _ _ _ /. *)
-  (* cbn. *)
-
-  Context (sigma: forall f, Sig_denote (Sigma f)).
   Context (r: ContextEnv.(env_t) R).
+  Context (sigma: forall f, Sig_denote (Sigma f)).
 
   Arguments wp_action {pos_t var_t fn_name_t} {reg_t ext_fn_t} {R Sigma} {REnv} r sigma
             {sig tau} _ !_ _ /.
 
+  Arguments interp_cycle_cps {pos_t var_t fn_name_t rule_name_t reg_t ext_fn_t}%type_scope {R Sigma}%function_scope {REnv} r (sigma rules)%function_scope !s / {A}%type_scope k : assert.
   Arguments interp_scheduler_cps {pos_t var_t fn_name_t rule_name_t reg_t ext_fn_t}%type_scope {R Sigma}%function_scope {REnv} r (sigma rules)%function_scope !s / {A}%type_scope k : assert.
   Arguments interp_rule_cps {pos_t var_t fn_name_t reg_t ext_fn_t}%type_scope {R Sigma}%function_scope {REnv} r sigma%function_scope !rl / {A}%type_scope k log : assert.
 
   Arguments wp_scheduler {pos_t var_t fn_name_t rule_name_t reg_t ext_fn_t}%type_scope {R Sigma}%function_scope {REnv} r (sigma rules)%function_scope !s / post : assert.
 
-  Lemma scheduler_correct :
-    exists l', interp_scheduler r sigma rls pipeline = l'.
-  Proof.
-    unfold pipeline.
-    (* unfold interp_scheduler, interp_scheduler', interp_rule; simpl. *)
-    (* rewrite <- interp_scheduler_cps_correct; simpl. *)
-    apply wp_scheduler_correct.
-    simpl.
-    Arguments logentry_app {T}%type_scope !l1 !l2 /: assert.
-    simpl.
-    destruct (Bits.single (getenv ContextEnv r queue_invalid)); simpl.
-    all: eexists; reflexivity.
-  Qed.
+  Arguments interp_cycle: simpl never.
+  Arguments id _ _ / : assert.
+
+  Declare Custom Entry context_mapping.
+
+  Notation "x  ->  y" :=
+    (CtxCons x y CtxEmpty)
+      (in custom context_mapping at level 80,
+          x constr at level 0, y constr at level 80,
+          no associativity).
+
+  Notation "x  ->  y ;  z" :=
+    (CtxCons x y z)
+      (in custom context_mapping at level 80,
+          x constr at level 0, y constr at level 80,
+          z custom context_mapping at level 80,
+          right associativity).
+
+  Notation "#{  x  }#" := (x) (at level 0, x custom context_mapping at level 200) : context.
+  Arguments env_t: simpl never.
+  Arguments vect: simpl never.
 
   Arguments wp_cycle {pos_t var_t fn_name_t rule_name_t reg_t ext_fn_t}%type_scope {R Sigma}%function_scope {REnv} r (sigma rules)%function_scope !s / post : assert.
 
@@ -131,13 +127,16 @@ Section correct.
 
   Arguments may_read /.
   Arguments may_write /.
+  Arguments logentry_app {T}%type_scope !l1 !l2 /: assert.
 
   Lemma cycle_correct :
     forall (init: ContextEnv.(env_t) R),
     exists l', Logs.commit_update init (TypedSemantics.interp_scheduler init sigma rls pipeline) = l'.
   Proof.
     intros; unfold pipeline.
-    Time unfold TypedSemantics.interp_scheduler, TypedSemantics.interp_scheduler', TypedSemantics.interp_rule; simpl. (* 11s *)
+    (* Time unfold TypedSemantics.interp_scheduler, *)
+    (* TypedSemantics.interp_scheduler', *)
+    (* TypedSemantics.interp_rule; simpl. (* 11s *) *)
   Abort.
 
   Lemma cycle_correct :
@@ -145,95 +144,195 @@ Section correct.
     exists l', CompactLogs.commit_update init (CompactSemantics.interp_scheduler init sigma rls pipeline) = l'.
   Proof.
     intros; unfold pipeline.
-    Time unfold CompactSemantics.interp_scheduler, CompactSemantics.interp_scheduler', CompactSemantics.interp_rule; simpl. (* 17s *)
+    (* Time unfold CompactSemantics.interp_scheduler, CompactSemantics.interp_scheduler', CompactSemantics.interp_rule; simpl. (* 17s *) *)
   Abort.
 
   Lemma cycle_correct :
     forall (init: ContextEnv.(env_t) R),
-    exists l', CompactLogs.commit_update init (CompactSemantics.interp_scheduler init sigma rls pipeline) = l'.
+    exists l', WP.interp_cycle init sigma rls pipeline = l'.
   Proof.
     intros; unfold pipeline.
     apply wp_cycle_correct.
-    Time simpl. (* 0.6s *)
+    Time simpl. (* 0.7s *)
 
-    destruct (Bits.single init.[queue_invalid]); simpl.
+    destruct (Bits.single init.[queue_empty]); simpl.
     - (* Initialization: nothing in the queue yet *)
       eexists; reflexivity.
     - (* Steady state: queue remains full *)
       eexists; reflexivity.
   Qed.
 
-
-
-
-
-
-
-  Lemma doG_correct :
-    Bits.single (ContextEnv.(getenv) r queue_invalid) = false ->
-    (* ContextEnv.(getenv) r queue_data = (sigma F) (ContextEnv.(getenv) r output_buffer) -> *)
-    exists l,
-      interp_rule r sigma log_empty (rls doG) = Some l /\
-      (ContextEnv.(getenv) l queue_invalid).(lwrite0) = Some Ob~1.
-  (* FIXME: which style is better; exists or match? *)
-  (* match interp_rule r sigma log_empty (rls doG) with *)
-  (* | Some l => *)
-  (*   ContextEnv.(getenv) l queue_invalid = [LE LogWrite P0 Ob~1; LE LogRead P0 tt] *)
-  (* | None => False *)
-  (* end. *)
+  Lemma scheduler_correct :
+    exists l', interp_scheduler r sigma rls pipeline = l'.
   Proof.
-    intros Hvalid.
-    unfold interp_rule.
-    (* Time simpl; rewrite Hvalid; simpl; eexists; split; reflexivity. *)
-    apply (wp_action_correct (rule_name_t := rule_name_t)).
-    Time simpl; rewrite Hvalid; simpl; eexists; split; reflexivity.
-  Qed.
-
-  Ltac autorew :=
-    repeat match goal with
-           | [ H: ?a = ?a' |- context[match ?b with _ => _ end] ] =>
-             unify a b; replace b with a' by assumption
-           (* The variant below isn't enough, because primitive projections are weird *)
-           (* | [ H: ?a = _ |- _ ] => *)
-           (*   match goal with (* Merging the two matches together results in a 20x slowdown *) *)
-           (*   | [  |- context[a] ] => rewrite !H *)
-           (*   end *)
-           end.
-
-  (* FIXME *)
-  Arguments may_read0 /.
-  Arguments may_read1 /.
-  Arguments may_write0 /.
-  Arguments may_write1 /.
-  Arguments vect_cons: simpl never.
-
-  Lemma doF_correct :
-    forall l,
-      let l_input_buffer := ContextEnv.(getenv) l input_buffer in
-      let l_queue_invalid := ContextEnv.(getenv) l queue_invalid in
-      let l_queue_data := ContextEnv.(getenv) l queue_data in
-      l_input_buffer.(lwrite0) = None ->
-      l_input_buffer.(lwrite1) = None ->
-      l_input_buffer.(lread1) = false ->
-      l_queue_data.(lread1) = false ->
-      l_queue_data.(lwrite0) = None ->
-      l_queue_data.(lwrite1) = None ->
-      l_queue_invalid.(lwrite1) = None ->
-      l_queue_invalid.(lwrite0) = Some Ob~1 ->
-      (* ContextEnv.(getenv) r queue_data = (sigma F) (ContextEnv.(getenv) r output_buffer) -> *)
-      exists l',
-        interp_rule r sigma l (rls doF) = Some l'.
-  Proof.
-    intros.
-    unfold interp_rule.
-    (* rewrite (interp_action_cps_correct_rev (rule_name_t := rule_name_t)). *)
-    (* simpl. *)
-    apply (wp_action_correct (rule_name_t := rule_name_t)).
+    unfold pipeline.
+    (* unfold interp_scheduler, interp_scheduler', interp_rule; simpl. *)
+    (* rewrite <- interp_scheduler_cps_correct; simpl. *)
+    apply wp_scheduler_correct.
     Time simpl.
-    Time autorew.
-    simpl.
-    eexists; split; reflexivity.
+    destruct (Bits.single (getenv ContextEnv r queue_empty)); simpl.
+    all: eexists; reflexivity.
   Qed.
+
+  Require Lists.Streams.
+  Declare Scope stream_scope.
+  Infix ":::" := Streams.Cons (at level 60, right associativity) : stream_scope.
+
+  Section Traces.
+    Open Scope stream_scope.
+
+    CoFixpoint iterate {A} (f: A -> A) (init: A) :=
+      init ::: iterate f (f init).
+
+    Lemma iterate_eqn {A} (f: A -> A) (init: A) :
+      iterate f init =
+      init ::: iterate f (f init).
+    Proof.
+      rewrite (Streams.unfold_Stream (iterate f init)) at 1; reflexivity.
+    Qed.
+
+    Lemma map_eqn {A B} (f: A -> B) (s: Streams.Stream A) :
+      Streams.map f s =
+      f (Streams.hd s) ::: Streams.map f (Streams.tl s).
+    Proof.
+      rewrite (Streams.unfold_Stream (Streams.map f s)) at 1; reflexivity.
+    Qed.
+
+    (* Spec: The stream of inputs *)
+    Definition spec_inputs :=
+      iterate (sigma NextInput) r.[input_buffer].
+
+    (* Spec: The stream of outputs *)
+    Definition spec_outputs :=
+      let composed x := sigma G (sigma F x) in
+      r.[output_buffer] ::: r.[output_buffer] ::: Streams.map composed spec_inputs.
+
+    Definition states_trace :=
+      iterate (fun r: env_t _ R => interp_cycle r sigma rls pipeline) r.
+
+    Definition observable_trace :=
+      Streams.map (fun r: env_t _ R => r.[output_buffer]) states_trace.
+
+    Import Streams.
+
+    Ltac autorew :=
+      (* The variant below isn't enough, because primitive projections are weird *)
+      (* | [ H: ?a = _ |- _ ] => *)
+      (*   match goal with (* Merging the two matches together results in a 20x slowdown *) *)
+      (*   | [  |- context[a] ] => rewrite !H *)
+      (*   end *)
+      repeat match goal with
+             | [ H: ?a = ?a' |- context[match ?b with _ => _ end] ] =>
+               unify a b; replace b with a' by assumption
+             end.               (* FIXME doesn't work here *)
+
+    Lemma interp_pipeline_eqn :
+      forall r: ContextEnv.(env_t) R,
+        r.[queue_empty] = Ob~0 ->
+        interp_cycle r sigma rls pipeline =
+        #{ queue_data -> sigma F (r.[input_buffer]);
+           output_buffer -> sigma G r.[queue_data];
+           input_buffer -> sigma NextInput r.[input_buffer];
+           queue_empty -> Ob~0 }#.
+    Proof.
+      clear r; intros r H0.
+      apply wp_cycle_correct.
+      simpl.
+      rewrite H0; reflexivity.
+    Qed.
+
+    Lemma interp_pipeline_eqn2 :
+      forall r: ContextEnv.(env_t) R,
+        r.[queue_empty] = Ob~0 ->
+        interp_cycle (interp_cycle r sigma rls pipeline) sigma rls pipeline =
+        #{ queue_data -> sigma F (sigma NextInput r.[input_buffer]);
+           output_buffer -> sigma G (sigma F r.[input_buffer]);
+           input_buffer -> sigma NextInput (sigma NextInput r.[input_buffer]);
+           queue_empty -> Ob~0 }#.
+    Proof.
+      clear r; intros r H0.
+      Opaque interp_cycle.
+      rewrite (interp_pipeline_eqn r) by assumption; simpl.
+      rewrite interp_pipeline_eqn by reflexivity; simpl.
+      reflexivity.
+    Qed.
+
+    Theorem correct_pipeline:
+      r.[queue_empty] = Ob~1 ->
+      Streams.EqSt observable_trace spec_outputs.
+    Proof.
+      intros Hqueue_empty.
+      unfold observable_trace, spec_outputs, states_trace.
+      rewrite iterate_eqn.
+      rewrite interp_cycle_cps_correct_rev.
+      simpl.
+      rewrite Hqueue_empty.
+      simpl.
+      rewrite iterate_eqn.
+      rewrite interp_cycle_cps_correct_rev.
+      simpl.
+      rewrite map_eqn; simpl.
+      rewrite map_eqn; simpl.
+      constructor; [reflexivity | simpl tl].
+      constructor; [reflexivity | simpl tl].
+    Abort.
+
+    Lemma doG_correct :
+      Bits.single (ContextEnv.(getenv) r queue_empty) = false ->
+      (* ContextEnv.(getenv) r queue_data = (sigma F) (ContextEnv.(getenv) r output_buffer) -> *)
+      exists l,
+        interp_rule r sigma log_empty (rls doG) = Some l /\
+        l.[queue_empty].(lwrite0) = Some Ob~1.
+    (* FIXME: which style is better; exists or match? *)
+    (* match interp_rule r sigma log_empty (rls doG) with *)
+    (* | Some l => *)
+    (*   ContextEnv.(getenv) l queue_empty = [LE LogWrite P0 Ob~1; LE LogRead P0 tt] *)
+    (* | None => False *)
+    (* end. *)
+    Proof.
+      intros Hvalid.
+      unfold interp_rule.
+      (* Time simpl.  rewrite Hvalid; simpl; eexists; split; reflexivity. *)
+      apply (wp_action_correct (rule_name_t := rule_name_t)).
+      Time simpl; rewrite Hvalid; simpl; eexists; split; reflexivity.
+    Qed.
+
+    (* FIXME *)
+    Arguments may_read0 /.
+    Arguments may_read1 /.
+    Arguments may_write0 /.
+    Arguments may_write1 /.
+    Arguments vect_cons: simpl never.
+
+    Arguments env_t : simpl never.
+
+    Lemma doF_correct :
+      forall l,
+        let l_input_buffer := ContextEnv.(getenv) l input_buffer in
+        let l_queue_empty := ContextEnv.(getenv) l queue_empty in
+        let l_queue_data := ContextEnv.(getenv) l queue_data in
+        l_input_buffer.(lwrite0) = None ->
+        l_input_buffer.(lwrite1) = None ->
+        l_input_buffer.(lread1) = false ->
+        l_queue_data.(lread1) = false ->
+        l_queue_data.(lwrite0) = None ->
+        l_queue_data.(lwrite1) = None ->
+        l_queue_empty.(lwrite1) = None ->
+        l_queue_empty.(lwrite0) = Some Ob~1 ->
+        (* ContextEnv.(getenv) r queue_data = (sigma F) (ContextEnv.(getenv) r output_buffer) -> *)
+        exists l',
+          interp_rule r sigma l (rls doF) = Some l'.
+    Proof.
+      intros.
+      unfold interp_rule.
+      (* rewrite (interp_action_cps_correct_rev (rule_name_t := rule_name_t)). *)
+      (* simpl. *)
+      apply (wp_action_correct (rule_name_t := rule_name_t)).
+      Time simpl.
+      Time autorew.
+      simpl.
+      eexists; split; reflexivity.
+    Qed.
 
 
 Definition external (r: rule_name_t) := false.
@@ -241,12 +340,20 @@ Definition external (r: rule_name_t) := false.
 Definition circuits :=
   compile_scheduler rules external pipeline.
 
+Definition r reg : R reg :=
+  match reg with
+  | queue_data => Bits.zero
+  | output_buffer => Bits.zero
+  | input_buffer => Bits.zero
+  | queue_empty => Ob~1
+  end.
+
 Definition circuits_result sigma :=
   interp_circuits (ContextEnv.(create) r) sigma circuits.
 
 Definition cpp_extfuns := "class extfuns {
 public:
-  static bits<32> stream(bits<32> lfsr) {
+  static bits<32> next_input(bits<32> lfsr) {
     return lfsr + bits<32>{1};
   }
 
@@ -261,7 +368,7 @@ public:
 
 Definition ext_fn_names fn :=
   match fn with
-  | Stream => "stream"
+  | NextInput => "next_input"
   | F => "f"
   | G => "g"
   end.
@@ -286,3 +393,10 @@ Definition package :=
 
 Definition prog := Interop.Backends.register package.
 Extraction "pipeline.ml" prog.
+
+
+  (* FIXME: is it a cbn bug that adding exclamation points below prevents reduction?*)
+  (* Probably due to implicits *)
+  (* Arguments may_read _ _ _ /. *)
+  (* Arguments may_write _ _ _ /. *)
+  (* cbn. *)
