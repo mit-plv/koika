5b503ff7e14e1d2da493fd48752530e4d3b2c081
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-12-17 17:05:35 -0500

    examples: Document collatz.v

--- examples/collatz.v
+++ examples/collatz.v
@@ -2,17 +2,21 @@
 Require Import Koika.Frontend.
 
 Module Collatz.
+  (*! We have one register ``r0``: !*)
   Inductive reg_t := r0.
+  (*! …and two rules ``divide`` and ``multiply``: !*)
   Inductive rule_name_t := divide | multiply.
 
   Definition logsz := 4.
   Notation sz := (pow2 logsz).
 
+  (*! ``r0`` is a register storing 2^4 = 16 bits: !*)
   Definition R r :=
     match r with
     | r0 => bits_t sz
     end.
 
+  (*! ``r0`` is initialized to 18: !*)
   Definition r idx : R idx :=
     match idx with
     | r0 => Bits.of_nat sz 18
@@ -22,6 +26,7 @@ Module Collatz.
     {{ fun times_three (bs: bits_t 16) : bits_t 16 =>
          (bs << Ob~1) + bs }}.
 
+  (*! Our first rule, ``divide``, reads from r0 and halves the result if it's even: !*)
   Definition _divide : uaction reg_t empty_ext_fn_t :=
     {{ let v := read0(r0) in
        let odd := v[Ob~0~0~0~0] in
@@ -30,6 +35,8 @@ Module Collatz.
        else
          fail }}.
 
+  (*! Our second rule, ``multiply``, reads the output of ``divide`` and
+      multiplies it by three and ads one if it is odd: !*)
   Definition _multiply : uaction reg_t empty_ext_fn_t :=
     {{ let v := read1(r0) in
        let odd := v[Ob~0~0~0~0] in
@@ -38,12 +45,13 @@ Module Collatz.
        else
          fail }}.
 
+  (*! The design's schedule defines the order in which rules should (appear to) run !*)
   Definition collatz : scheduler :=
     divide |> multiply |> done.
 
   Definition cr := ContextEnv.(create) r.
 
-  (* Typechecking  *)
+  (*! Rules are written in an untyped language, so we need to typecheck them: !*)
   Definition rules :=
     tc_rules R empty_Sigma
              (fun r => match r with
@@ -51,18 +59,34 @@ Module Collatz.
                     | multiply => _multiply
                     end).
 
-  Definition cycle_log :=
-    tc_compute (interp_scheduler cr empty_sigma rules collatz).
+  (*! And now we can compute results: uncomment the ``Print`` commands below to show results. !*)
 
   Definition divide_result :=
     tc_compute (interp_action cr empty_sigma CtxEmpty log_empty log_empty
                               (rules divide)).
 
+  (*! The output of ``divide`` when ``r0 = 18``: a write to ``r0`` with value ``0b1001 = 9``: !*)
+  (* Print divide_result. *)
+
   Definition multiply_result :=
-    tc_compute (interp_action cr empty_sigma CtxEmpty log_empty log_empty (rules multiply)).
+    tc_compute
+      (let '(log, _, _) := must divide_result in
+       interp_action cr empty_sigma CtxEmpty log_empty log (rules multiply)).
+
+  (*! The output of ``multiply`` when ``r0 = 9``: a write to ``r0`` with value ``0b11100 = 28``: !*)
+  (* Print multiply_result. *)
+
+  Definition cycle_log :=
+    tc_compute (interp_scheduler cr empty_sigma rules collatz).
+
+  (*! The complete log over the first cycle: two writes to ``r0`` !*)
+  (* Print cycle_log. *)
 
   Definition result :=
     tc_compute (commit_update cr cycle_log).
+
+  (*! The values of the registers after the first cycle completes: ``r0 = 28`` !*)
+  (* Print result. *)
 
   Definition external (r: rule_name_t) := false.
 
@@ -72,9 +96,13 @@ Module Collatz.
   Definition circuits_result :=
     tc_compute (interp_circuits empty_sigma circuits (lower_r (ContextEnv.(create) r))).
 
+  (*! The same values computed by running compiled circuits: ``r0 = 28`` !*)
+  (* Print circuits_result. *)
+
   Example test: circuits_result = Environments.map _ (fun _ => bits_of_value) result :=
     eq_refl.
 
+  (*! This package configures compilation to C++ with Cuttlesim and Verilog with Kôika's verified compiler: !*)
   Definition package :=
     {| ip_koika := {| koika_reg_types := R;
                      koika_reg_init := r;
@@ -90,4 +118,5 @@ Module Collatz.
        ip_verilog := {| vp_ext_fn_specs := empty_ext_fn_props |} |}.
 End Collatz.
 
+(*! This is the entry point used by the compiler: !*)
 Definition prog := Interop.Backends.register Collatz.package.
