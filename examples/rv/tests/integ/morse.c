ae7ea869e1cd7a0dfc73655cc2ccc87cb67027bc
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-03-07 21:51:28 -0500

    example/rv: Generalize morse.c test

--- examples/rv/tests/integ/morse.c
+++ examples/rv/tests/integ/morse.c
@@ -1,41 +1,82 @@
 #include "../mmio.h"
 
-const char* pattern = "-.- --- .. -.- .-";
+typedef struct {
+  char letter;
+  const char* code;
+} pair;
 
-void blink(char c, int DOT) {
-  int on, duration;
+const char* morse_code[26] = {
+  ".-",   // 'A'
+  "-...", // 'B'
+  "-.-.", // 'C'
+  "-..",  // 'D'
+  ".",    // 'E'
+  "..-.", // 'F'
+  "--.",  // 'G'
+  "....", // 'H'
+  "..",   // 'I'
+  ".---", // 'J'
+  "-.-",  // 'K'
+  ".-..", // 'L'
+  "--",   // 'M'
+  "-.",   // 'N'
+  "---",  // 'O'
+  ".--.", // 'P'
+  "--.-", // 'Q'
+  ".-.",  // 'R'
+  "...",  // 'S'
+  "-",    // 'T'
+  "..-",  // 'U'
+  "...-", // 'V'
+  ".--",  // 'W'
+  "-..-", // 'X'
+  "-.--", // 'Y'
+  "--..", // 'Z'
+};
 
-  int DASH = (3 * DOT);
-  int SPACE = DOT;
-  int LETTER_SPACE = (3 * DOT);
-
-  switch (c) {
-  case '.':
-    on = 1, duration = DOT;
-    break;
-  case '-':
-    on = 1, duration = DASH;
-    break;
-  default:
-    on = 0, duration = LETTER_SPACE;
-    break;
-  }
-
+void beep(int on, int duration) {
   putled(on);
   wait(duration);
-  putled(0);
-  wait(SPACE);
+}
+
+const char* morse_of_char(const char c) {
+  return
+    ('a' <= c && c <= 'z') ? morse_code[c - 'a'] :
+    ('A' <= c && c <= 'Z') ? morse_code[c - 'A'] :
+    0;
+}
+
+void tx1(const char letter, const int DOT) {
+  int DASH = 3 * DOT;
+  int SPACE = DOT;
+  int WORD_SPACE = 7 * DOT;
+  int LETTER_SPACE = 3 * DOT;
+
+  const char* beeps = morse_of_char(letter);
+
+  if (beeps) {
+    while (*beeps) {
+      switch (*beeps++) {
+      case '.': beep(1, DOT); break;
+      case '-': beep(1, DASH); break;
+      }
+      beep(0, SPACE);
+    }
+    wait(LETTER_SPACE - SPACE);
+  } else {
+    beep(0, WORD_SPACE);
+  }
+}
+
+void tx(const char* message, const int DOT) {
+  while (*message)
+    tx1(*message++, DOT);
 }
 
 int main() {
-  int DOT = host_is_fpga() ? 600000 : 20000;
-  int WORD_SPACE = (7 * DOT);
+  int DOT = host_is_fpga() ? 200000 : 5000;
 
   do {
-    wait(WORD_SPACE);
-    const char* p = pattern;
-    while (*p)
-      blink(*p++, DOT);
-    putln();
+    tx("KOIKA ", DOT);
   } while (host_is_fpga());
 }
